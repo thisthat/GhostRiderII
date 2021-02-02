559e4093a9d5da58614eae96731f5151c9178c2b
JackDC-12 <jackoid89@gmail.com>
2021-02-02 19:59:58 +0100

    Update Offsets.h

--- GhostRaiderII/Offsets.h
+++ GhostRaiderII/Offsets.h
@@ -9,4 +9,25 @@ namespace Offsets {
 		static constexpr uintptr_t renderLaraFunc = 0x419DD0;
 		static constexpr uintptr_t title = 0x11BD90;
 	};
+	
+	class Steam_UK {
+	public:
+		static constexpr uintptr_t loadLevelFunc = 0x43C790;
+		static constexpr uintptr_t renderLaraFunc = 0x419DD0;
+		static constexpr uintptr_t title = 0x11BD90;
+	};
+	
+	class Steam_EPC {
+	public:
+		static constexpr uintptr_t loadLevelFunc = 0x43C790;
+		static constexpr uintptr_t renderLaraFunc = 0x419DD0;
+		static constexpr uintptr_t title = 0x11BD90;
+	};
+	
+	class Steam_MULTI {
+	public:
+		static constexpr uintptr_t loadLevelFunc = 0x43C790;
+		static constexpr uintptr_t renderLaraFunc = 0x419DD0;
+		static constexpr uintptr_t title = 0x11BD90;
+	};
 }
